99322e29c72c3a044bbc717e9d4551f280d02eff
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-07-16 18:38:47 +0200

    adding buttons to the story board so u can test the buttons with the print message

--- SOLID Principles/SingleResponsibiltyPrinciple/SingleResponsibiltyPrinciple/ViewController.swift
+++ SOLID Principles/SingleResponsibiltyPrinciple/SingleResponsibiltyPrinciple/ViewController.swift
@@ -8,17 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    //MARK:- Properties
+
     let car = Car()
     
+    
+    //MARK:- View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        car.accelerate()
-        car.addFuel()
-        car.turnOnAc()
     }
 
+    //MARK:- Actions
+    
+    @IBAction func carControlls(_ sender: UIButton) {
+        switch sender.tag {
+        case 0:
+            car.addFuel()
+        case 1:
+            car.accelerate()
+        case 2:
+            car.turnOnAc()
+        default:
+            print("Nothing")
+        }
+    }
+    
+    
 
 }
 
